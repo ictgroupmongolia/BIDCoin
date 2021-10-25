36a17a4cb0f741430ca98834b5ca6aa116118dc7
Khaschuluu Munkhbayar <khaschuluu@ictgroup.mn>
2021-10-25 14:07:01 +0800

    mint to different address

--- BIDCoin_flat.sol
+++ BIDCoin_flat.sol
@@ -490,32 +490,12 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
 
 // File: contracts/BIDCoin.sol
 
-// // contracts/BIDCoin.sol
-// pragma solidity ^0.8.0;
-
-// import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
-
-// contract BIDCoin is ERC777 {
-//     constructor(uint256 initialSupply, address[] memory defaultOperators)
-//         ERC777("BIDCoin", "BID", defaultOperators)
-//     {
-//         _mint(msg.sender, initialSupply, "", "");
-//     }
-
-//     function operatorBurn(address, uint256, bytes calldata, bytes calldata) public virtual override {
-//         revert();
-//     }
-
-//     function burn(uint256, bytes memory) public virtual override {
-//         revert();
-//     }
-// }
-
 // contracts/GLDToken.sol
+pragma solidity 0.8.0;
 
 
 contract BIDCoin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("BIDCoin", "BID") {
-        _mint(msg.sender, initialSupply);
+    constructor(address _mintTo, uint256 initialSupply) ERC20("BIDCoin", "BID") {
+        _mint(_mintTo, initialSupply);
     }
 }
--- contracts/BIDCoin.sol
+++ contracts/BIDCoin.sol
@@ -1,11 +1,11 @@
 // contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BIDCoin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("BIDCoin", "BID") {
-        _mint(msg.sender, initialSupply);
+    constructor(address _mintTo, uint256 initialSupply) ERC20("BIDCoin", "BID") {
+        _mint(_mintTo, initialSupply);
     }
 }
