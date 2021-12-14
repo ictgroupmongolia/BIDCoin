828733f7a90b439da05b7d1eb5f806a903c15320
developerictgroup <84491333+developerictgroup@users.noreply.github.com>
2021-12-14 12:13:04 +0800

    final distribution

--- contracts/BIDCoin.sol
+++ contracts/BIDCoin.sol
@@ -1,112 +1,101 @@
+// contracts/BIDCoin.sol
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
-
-// Author: Khashkhuu 'Xass1on' Gankhuyag
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/TokenTimelock.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract BIDCoin is ERC20, Ownable {
     using SafeMath for uint256;
-    TokenTimelock public teamTimelock1;
-    TokenTimelock public teamTimelock2;
-    TokenTimelock public teamTimelock3;
-    TokenTimelock public teamTimelock4;
-    TokenTimelock public teamTimelock5;
+    TokenTimelock public timelockProjectOwnerReserve;
 
-    uint256 constant _totalSupply = 3 * 10**10;
+    uint256 constant _totalSupply = 1.6 * 10**11;
 
-    uint256 constant _tokenToOffer = 1.5 * 10**10;
+    uint256 constant _tokenToOffer = 4 * 10**10;
+    uint256 constant _tokenFintech = 3 * 10**9;
 
-    uint256 constant _tokenTeamReserve1 = 1.2 * 10**9;
-    uint256 constant _tokenTeamReserve2 = 1.2 * 10**9;
-    uint256 constant _tokenTeamReserve3 = 1.2 * 10**9;
-    uint256 constant _tokenTeamReserve4 = 1.2 * 10**9;
-    uint256 constant _tokenTeamReserve5 = 1.2 * 10**9;
+    uint256 constant _tokenPromotion = 1.5 * 10**9;
+    uint256 constant _tokenManagementReserve = 14731649102;
 
-    uint256 constant _tokenLoyalty = 3 * 10**9;
-    uint256 constant _tokenMarketing = 3 * 10**9;
-    uint256 constant _tokenCooperation = 3 * 10**9;
+    uint256 constant _tokenCooperation = 1 * 10**9;
+    uint256 constant _tokenListing = 4 * 10**10;
+
+    uint256 constant _tokenCivilResponsibility = 5 * 10**8;
+    uint256 constant _tokenProjectOwnerReserve = 1.5 * 10**10;
+
+    uint256 constant _tokenStrategicReserve = 1.5 * 10**10;
+    uint256 constant _tokenBurn = 29268350898;
 
     string constant _tokenName = "BIDCoin";
     string constant _tokenSymbol = "BID";
 
-    address constant _tokenToOfferAddress =
-        0x75a0Cc530AF5fF6D3d4611CaF6422Ebe25CedBf7;
+    address constant _tokenOfferAddress =
+        0xa4627D4AEeEf81e1cb7EB63d0E6Fd2f4f7682d93;
+    address constant _tokenFintechAddress =
+        0x02e0bDa9c03C7769D5d7d7f5034EC192b3077121;
 
-    address constant _tokenTeamReserveAddress1 =
-        0xD6bE95f9898Cfe0e942F43e7417a76EBba08F1BA;
-    address constant _tokenTeamReserveAddress2 =
-        0xAD5043D495dF6f7103c04911d0aE1d028F721367;
-    address constant _tokenTeamReserveAddress3 =
-        0x1bBDAf6Bf3b33AAf1181F520c403D98c0deB994E;
-    address constant _tokenTeamReserveAddress4 =
-        0x38e1a68C547171F323AAc8e7A44F2de4dD531dD5;
-    address constant _tokenTeamReserveAddress5 =
-        0x166d5057FbD4881872DD14dB5a662F7dC52c9661;
-
-    address constant _tokenLoyaltyAddress =
-        0x7FC663904baB33E9bA5c8306ffb211b91c131040;
-
-    address constant _tokenMarketingAddress =
-        0xb065f040858c051e0999855a9542ce93A4c1b725;
+    address constant _tokenPromotionAddress =
+        0xf797D74f1313E014660087dDaBB3E12D25Af4921;
+    address constant _tokenManagementReserveAddress =
+        0x47d80e39875c23AFb8990AecD280bd14de9901a5;
 
     address constant _tokenCooperationAddress =
-        0x1ae24Fc66211c9A8bbFa618839B9F9800BCB1eD5;
+        0xf5774463ACfaD5539273986f070FeD7e5406DF4C;
+    address constant _tokenListingAddress =
+        0x16F45b114bfe88e7Dc55d49f5631a991c6b265d9;
+
+    address constant _tokenCivilResponsebilityAddress =
+        0x236c0B2EC8A7254A15BD3b36ceFeC7de00BdBA93;
+    address constant _tokenProjectOwnerReserveAddress =
+        0xD8Adba8A28E7E23fb67A0b254D993702324ef800;
+
+    address constant _tokenStrategicReserveAddress =
+        0x1Ab16Eed90A45B1852821Cb638DfB273A62255E0;
+    address constant _tokenBurnAddress =
+        0x22f335c3b53867686FA6b54b8A9D5c5CaFF20516;
 
     constructor() ERC20(_tokenName, _tokenSymbol) {
-        // ICO
-        _mint(_tokenToOfferAddress, _tokenToOffer * 10**18);
+        // IEO
+        _mint(_tokenOfferAddress, _tokenToOffer * 10**18);
 
-        // Team reserve
-        teamTimelock1 = new TokenTimelock(
-            this,
-            _tokenTeamReserveAddress1,
-            block.timestamp + 60 * 60 * 24 * 365 * 1
+        // Fintech reward
+        _mint(_tokenFintechAddress, _tokenFintech * 10**18);
+
+        // Product promotion
+        _mint(_tokenPromotionAddress, _tokenPromotion * 10**18);
+
+        // Management, marketing reserve
+        _mint(_tokenManagementReserveAddress, _tokenManagementReserve * 10**18);
+
+        // Cooperation reward
+        _mint(_tokenCooperationAddress, _tokenCooperation * 10**18);
+
+        // Exchange listing
+        _mint(_tokenListingAddress, _tokenListing * 10**18);
+
+        // Civil responsibility
+        _mint(
+            _tokenCivilResponsebilityAddress,
+            _tokenCivilResponsibility * 10**18
         );
-        teamTimelock2 = new TokenTimelock(
+
+        // Project owners reserve
+        timelockProjectOwnerReserve = new TokenTimelock(
             this,
-            _tokenTeamReserveAddress2,
-            block.timestamp + 60 * 60 * 24 * 365 * 2
-        );
-        teamTimelock3 = new TokenTimelock(
-            this,
-            _tokenTeamReserveAddress3,
+            _tokenProjectOwnerReserveAddress,
             block.timestamp + 60 * 60 * 24 * 365 * 3
         );
-        teamTimelock4 = new TokenTimelock(
-            this,
-            _tokenTeamReserveAddress4,
-            block.timestamp + 60 * 60 * 24 * 365 * 4
+        _mint(
+            address(timelockProjectOwnerReserve),
+            _tokenProjectOwnerReserve * 10**18
         );
-        teamTimelock5 = new TokenTimelock(
-            this,
-            _tokenTeamReserveAddress5,
-            block.timestamp + 60 * 60 * 24 * 365 * 5
-        );
-        _mint(address(teamTimelock1), _tokenTeamReserve1 * 10**18);
-        _mint(address(teamTimelock2), _tokenTeamReserve2 * 10**18);
-        _mint(address(teamTimelock3), _tokenTeamReserve3 * 10**18);
-        _mint(address(teamTimelock4), _tokenTeamReserve4 * 10**18);
-        _mint(address(teamTimelock5), _tokenTeamReserve5 * 10**18);
 
-        // Loyalty
-        _mint(_tokenLoyaltyAddress, _tokenLoyalty * 10**18);
+        // Strategic reserve
+        _mint(_tokenStrategicReserveAddress, _tokenStrategicReserve * 10**18);
 
-        // Marketing
-        _mint(_tokenMarketingAddress, _tokenMarketing * 10**18);
-
-        // Co-operation
-        _mint(_tokenCooperationAddress, _tokenCooperation * 10**18);
-    }
-
-    fallback() external payable {
-        revert();
-    }
-
-    receive() external payable {
-        revert();
+        // Burn
+        _mint(_tokenBurnAddress, _tokenBurn * 10**18);
     }
 }
